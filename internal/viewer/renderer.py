332bc473d2d18f5b647baf1c0e2ecd30453b7654
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2023-10-25 15:56:51 +0800

    add load ply support to viewer

---
+++ internal/viewer/renderer.py
@@ -0,0 +1,24 @@
+from internal.models.gaussian_model_simplified import GaussianModelSimplified
+import internal.renderers as renderers
+
+
+class ViewerRenderer:
+    def __init__(
+            self,
+            gaussian_model: GaussianModelSimplified,
+            renderer: renderers.Renderer,
+            background_color,
+    ):
+        super().__init__()
+
+        self.gaussian_model = gaussian_model
+        self.renderer = renderer
+        self.background_color = background_color
+
+    def get_outputs(self, camera, scaling_modifier: float = 1.):
+        return self.renderer(
+            camera,
+            self.gaussian_model,
+            self.background_color,
+            scaling_modifier=scaling_modifier,
+        )["render"]
