91cb950d6f89621112ccb669faf7af289cdf4a5d
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2023-11-26 00:37:42 +0800

    add test cases

--- tests/deformable_model_test.py
+++ tests/deformable_model_test.py
@@ -4,6 +4,7 @@ from internal.utils.network_factory import NetworkFactory
 
 from internal.encodings.positional_encoding import PositionalEncoding
 from internal.models.deform_model import DeformModel
+from internal.models.vanilla_deform_model import VanillaDeformNetwork
 
 
 class DeformableModelTestCase(unittest.TestCase):
@@ -59,6 +60,75 @@ class DeformableModelTestCase(unittest.TestCase):
         self.assertEqual(len(dn.output_linear), 6)
         self.assertEqual(dn.skip_layers[0][0].in_features, 84)
         print(dn)
+
+    def test_synthetic_scene_output(self):
+        vanilla_model = VanillaDeformNetwork(is_blender=True)
+        vanilla_model.load_state_dict(torch.load("../../../src/Deformable-3D-Gaussians/output/lego/deform/iteration_40000/deform.pth"))
+        vanilla_model = vanilla_model.to("cuda")
+
+        checkpoint = torch.load("../outputs/D-NeRF_lego/baseline-1125/checkpoints/epoch=800-step=40000.ckpt")
+        model = checkpoint["hyper_parameters"]["renderer"].deform_model
+
+        with torch.no_grad():
+            input = checkpoint["state_dict"]["gaussian_model._xyz"].to("cuda")
+            time = torch.zeros((input.shape[0], 1), dtype=torch.float, device=input.device)
+            vanilla_outputs = vanilla_model(input, time)
+            outputs = model(input, time)
+
+            for i in range(len(vanilla_outputs)):
+                self.assertFalse(torch.all(vanilla_outputs[i] == outputs[i]))
+
+        model.embed_time_fn.timenet.load_state_dict(vanilla_model.timenet.state_dict())
+        for i in range(5):
+            model.skip_layers[0][2 * i].load_state_dict(vanilla_model.linear[i].state_dict())
+        for i in range(3):
+            model.output_linear[2 * i].load_state_dict(vanilla_model.linear[5 + i].state_dict())
+        model.gaussian_warp[0].load_state_dict(vanilla_model.gaussian_warp.state_dict())
+        model.gaussian_rotation[0].load_state_dict(vanilla_model.gaussian_rotation.state_dict())
+        model.gaussian_scaling[0].load_state_dict(vanilla_model.gaussian_scaling.state_dict())
+
+        with torch.no_grad():
+            input = checkpoint["state_dict"]["gaussian_model._xyz"].to("cuda")
+            time = torch.zeros((input.shape[0], 1), dtype=torch.float, device=input.device)
+            vanilla_outputs = vanilla_model(input, time)
+            outputs = model(input, time)
+
+            for i in range(len(vanilla_outputs)):
+                self.assertTrue(torch.all(vanilla_outputs[i] == outputs[i]))
+
+    def test_real_scene_output(self):
+        vanilla_model = VanillaDeformNetwork()
+        vanilla_model.load_state_dict(torch.load("../../../src/Deformable-3D-Gaussians/output/mochi-high-five/deform/iteration_40000/deform.pth"))
+        vanilla_model = vanilla_model.to("cuda")
+
+        checkpoint = torch.load("../outputs/DYCheck-mochi-high-five/2x-network_updated-step_4/checkpoints/epoch=889-step=40000.ckpt")
+        model = checkpoint["hyper_parameters"]["renderer"].deform_model
+
+        with torch.no_grad():
+            input = checkpoint["state_dict"]["gaussian_model._xyz"].to("cuda")
+            time = torch.zeros((input.shape[0], 1), dtype=torch.float, device=input.device)
+            vanilla_outputs = vanilla_model(input, time)
+            outputs = model(input, time)
+
+            for i in range(len(vanilla_outputs)):
+                self.assertFalse(torch.all(vanilla_outputs[i] == outputs[i]))
+
+        for i in range(5):
+            model.skip_layers[0][2 * i].load_state_dict(vanilla_model.linear[i].state_dict())
+        for i in range(3):
+            model.output_linear[2 * i].load_state_dict(vanilla_model.linear[5 + i].state_dict())
+        model.gaussian_warp[0].load_state_dict(vanilla_model.gaussian_warp.state_dict())
+        model.gaussian_rotation[0].load_state_dict(vanilla_model.gaussian_rotation.state_dict())
+        model.gaussian_scaling[0].load_state_dict(vanilla_model.gaussian_scaling.state_dict())
+
+        with torch.no_grad():
+            input = checkpoint["state_dict"]["gaussian_model._xyz"].to("cuda")
+            time = torch.zeros((input.shape[0], 1), dtype=torch.float, device=input.device)
+            vanilla_outputs = vanilla_model(input, time)
+            outputs = model(input, time)
+
+            for i in range(len(vanilla_outputs)):
+                self.assertTrue(torch.all(vanilla_outputs[i] == outputs[i]))
 
 
 if __name__ == '__main__':
