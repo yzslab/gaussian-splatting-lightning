020c21c98d186f0e85c19355eab9db0e80d1f085
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-05-12 17:43:56 +0800

    Add features_extra to simplifiedGaussianModelManager
    (cherry picked from commit c8cd6681535149fa1a7699fbf2a3413cef1eb54e)

--- internal/models/simplified_gaussian_model_manager.py
+++ internal/models/simplified_gaussian_model_manager.py
@@ -39,6 +39,7 @@ class SimplifiedGaussianModelManager:
         self._features = torch.zeros([total_gaussian_num] + list(simplified_gaussian_models[0].get_features.shape[1:]), **tensor_initialize_params)
         self._scaling = torch.zeros((total_gaussian_num, 3), **tensor_initialize_params)
         self._rotation = torch.zeros((total_gaussian_num, 4), **tensor_initialize_params)
+        self._features_extra = torch.zeros((total_gaussian_num, simplified_gaussian_models[0].get_features_extra.shape[-1]), **tensor_initialize_params)
 
         # merge gaussians
         for idx, model in enumerate(simplified_gaussian_models):
@@ -48,6 +49,7 @@ class SimplifiedGaussianModelManager:
             self._features[begin:end] = model.get_features.to(device)
             self._scaling[begin:end] = model.get_scaling.to(device)
             self._rotation[begin:end] = model.get_rotation.to(device)
+            self._features_extra[begin:end] = model.get_features_extra.to(device)
 
         self.max_sh_degree = simplified_gaussian_models[0].max_sh_degree
         self.active_sh_degree = simplified_gaussian_models[0].max_sh_degree
@@ -158,6 +160,10 @@ class SimplifiedGaussianModelManager:
     @property
     def get_opacity(self):
         return self._opacity
+
+    @property
+    def get_features_extra(self):
+        return self._features_extra
 
     def delete_gaussians(self, mask: torch.tensor):
         # delete form each model, build new indices
