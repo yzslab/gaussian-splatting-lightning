08dae968cf39d6ca39a9fe3e9373ace305d43d56
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-06-30 13:02:44 +0800

    clamp N: https://github.com/ubc-vision/3dgs-mcmc/commit/1926e2c00798fe36ecf30d605a919d9a00d1eea5

--- internal/density_controllers/mcmc_density_controller.py
+++ internal/density_controllers/mcmc_density_controller.py
@@ -115,7 +115,8 @@ class MCMCDensityControllerImpl(DensityControllerImpl):
             gaussian_model._xyz.add_(noise)
 
     def compute_relocation(self, opacity_old, scale_old, N) -> Tuple[torch.Tensor, torch.Tensor]:
-        assert torch.all(N <= self.config.N_max)  # whether such a check is necessary?
+        # assert torch.all(N <= self.config.N_max)  # whether such a check is necessary?
+        N.clamp_(min=1., max=self.config.N_max - 1)
         return compute_relocation(
             opacity_old,
             scale_old,
