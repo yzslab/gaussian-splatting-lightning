55d223a6d748de5459978801767b5d126f27a75d
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-07-12 17:46:08 +0800

    Merge optimizer states

--- utils/merge_distributed_ckpts.py
+++ utils/merge_distributed_ckpts.py
@@ -1,10 +1,6 @@
-import add_pypath
 import os
 import argparse
-import torch
 from tqdm import tqdm
-
-import internal.renderers.gsplat_renderer
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="Path to the model output directory")
@@ -13,6 +9,7 @@ args = parser.parse_args()
 checkpoint_dir = os.path.join(args.path, "checkpoints")
 
 # find max iteration
+print("Searching checkpoint files...")
 max_iteration = -1
 checkpoint_files = []
 for i in os.listdir(checkpoint_dir):
@@ -31,9 +28,15 @@ for i in os.listdir(checkpoint_dir):
 assert len(checkpoint_files) > 0
 
 print(checkpoint_files)
+
+import add_pypath
+import torch
+
 param_list_key_by_name = {}
 extra_param_list_key_by_name = {}
-for i in tqdm(checkpoint_files):
+optimizer_state_exp_avg_list_key_by_index = {}
+optimizer_state_exp_avg_sq_list_key_by_index = {}
+for i in tqdm(checkpoint_files, desc="Loading checkpoints"):
     ckpt = torch.load(os.path.join(checkpoint_dir, i), map_location="cpu")
     for i in ckpt["state_dict"]:
         if i.startswith("gaussian_model."):
@@ -45,16 +48,26 @@ for i in tqdm(checkpoint_files):
             continue
         extra_param_list_key_by_name.setdefault(i, []).append(ckpt["gaussian_model_extra_state_dict"][i])
 
-    # TODO: merge optimizers
+    assert len(ckpt["optimizer_states"]) == 1
+    for i in ckpt["optimizer_states"][0]["state"]:
+        optimizer_state_exp_avg_list_key_by_index.setdefault(i, []).append(ckpt["optimizer_states"][0]["state"][i]["exp_avg"])
+        optimizer_state_exp_avg_sq_list_key_by_index.setdefault(i, []).append(ckpt["optimizer_states"][0]["state"][i]["exp_avg_sq"])
 
+print("Merging Gaussians...")
 for i in param_list_key_by_name:
     ckpt["state_dict"][i] = torch.concat(param_list_key_by_name[i], dim=0)
 for i in extra_param_list_key_by_name:
     ckpt["gaussian_model_extra_state_dict"][i] = torch.concat(extra_param_list_key_by_name[i], dim=0)
-ckpt["optimizer_states"] = []
+print("Merging optimizers...")
+for i in optimizer_state_exp_avg_list_key_by_index.keys():
+    ckpt["optimizer_states"][0]["state"][i]["exp_avg"] = torch.concat(optimizer_state_exp_avg_list_key_by_index[i], dim=0)
+    ckpt["optimizer_states"][0]["state"][i]["exp_avg_sq"] = torch.concat(optimizer_state_exp_avg_sq_list_key_by_index[i], dim=0)
+
+import internal.renderers.gsplat_renderer
 
 ckpt["hyper_parameters"]["renderer"] = internal.renderers.gsplat_renderer.GSPlatRenderer()
 
 output_path = os.path.join(checkpoint_dir, checkpoint_files[0][:checkpoint_files[0].rfind("-")] + ".ckpt")
+print("Saving...")
 torch.save(ckpt, output_path)
 print(f"Saved to '{output_path}'")
