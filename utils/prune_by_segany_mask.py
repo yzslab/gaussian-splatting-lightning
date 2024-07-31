0ff87a4b2d26e927bdfc8b1da64e25775278d9c3
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-07-31 14:51:18 +0800

    Assert output file not exists

--- utils/prune_by_segany_mask.py
+++ utils/prune_by_segany_mask.py
@@ -1,4 +1,5 @@
 import add_pypath
+import os
 import argparse
 import torch
 from internal.utils.gaussian_model_loader import GaussianModelLoader
@@ -12,6 +13,7 @@ args = parser.parse_args()
 
 assert args.model != args.output
 assert args.output.endswith(".ply")
+assert os.path.exists(args.output) is False
 
 model, _ = GaussianModelLoader.search_and_load(args.model, device="cpu", eval_mode=True, pre_activate=False)
 mask = torch.load(args.mask, map_location="cpu")["mask"]
