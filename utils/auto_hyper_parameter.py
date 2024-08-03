6ea879e1ff86f18574e03d98fa67928ef1e61feb
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-08-03 15:14:28 +0800

    Fix auto_hyper_parameter.py

--- utils/auto_hyper_parameter.py
+++ utils/auto_hyper_parameter.py
@@ -1,66 +1,52 @@
-import add_pypath
 import argparse
 
-from internal.configs.optimization import OptimizationParams
-from internal.density_controllers.vanilla_density_controller import VanillaDensityController
-
-SCALABEL_PARAMS = [
-    "position_lr_max_steps",
-    "feature_extra_lr_max_steps",
-]
-
-DENSITY_CONTROLLER_SCALABLE_PARAMS = [
-    "densification_interval",
-    "opacity_reset_interval",
-    "densify_from_iter",
-    "densify_until_iter",
-]
+SCALABEL_PARAMS = {
+    "model.gaussian.optimization.means_lr_scheduler.init_args.max_steps": 30_000,
+    "model.density.densification_interval": 100,
+    "model.density.opacity_reset_interval": 3_000,
+    "model.density.densify_from_iter": 500,
+    "model.density.densify_until_iter": 15_000,
+}
 
 EXTRA_EPOCH_SCALABLE_STEP_PARAMS = [
-    "position_lr_max_steps",
-    "feature_extra_lr_max_steps",
+    "model.gaussian.optimization.means_lr_scheduler.init_args.max_steps",
 ]
 
 
-def auto_hyper_parameter(n: int, base: int = 300, extra_epoch: int = 0):
-    optimization_params = OptimizationParams()
-    density_controller_params = VanillaDensityController()
-
+def auto_hyper_parameter(
+        n: int,
+        base: int = 300,
+        extra_epoch: int = 0,
+        scalable_params: dict = SCALABEL_PARAMS,
+        extra_epoch_scalable_params: dict = EXTRA_EPOCH_SCALABLE_STEP_PARAMS,
+):
     scale_up = max(n / base, 1)
 
     extra_steps = 0
     if extra_epoch > 0:
         extra_steps = extra_epoch * max(n, base)
 
-    def scale_params(param_dataclass, scalable_params, extra_epoch_scalable_step_params):
-        for attr in scalable_params:
-            value = round(getattr(param_dataclass, attr) * scale_up)
-            if attr in extra_epoch_scalable_step_params:
+    def scale_params(scalable_params, extra_epoch_scalable_step_params):
+        scaled_params = {}
+        for name, value in scalable_params.items():
+            value = round(value * scale_up)
+            if name in extra_epoch_scalable_step_params:
                 value += extra_steps
-            setattr(
-                param_dataclass,
-                attr,
-                value,
-            )
 
-    scale_params(optimization_params, SCALABEL_PARAMS, EXTRA_EPOCH_SCALABLE_STEP_PARAMS)
-    scale_params(density_controller_params, DENSITY_CONTROLLER_SCALABLE_PARAMS, [])
+            scaled_params[name] = value
+        return scaled_params
 
-    return round(30_000 * scale_up) + extra_steps, optimization_params, density_controller_params, scale_up
+    return round(30_000 * scale_up) + extra_steps, scale_params(scalable_params, extra_epoch_scalable_params), scale_up
 
 
-def to_command_args(max_steps: int, optimization_params: OptimizationParams, density_controller_params):
+def to_command_args(max_steps: int, scale_params):
     args = [
         "--max_steps",
         str(max_steps),
     ]
 
-    for i in SCALABEL_PARAMS:
-        args.append(f"--model.gaussian.optimization.{i}")
-        args.append(str(getattr(optimization_params, i)))
-    for i in DENSITY_CONTROLLER_SCALABLE_PARAMS:
-        args.append(f"--model.density.{i}")
-        args.append(str(getattr(density_controller_params, i)))
+    for i, v in scale_params.items():
+        args.append("--{} {}".format(i, v))
 
     return args
 
@@ -72,5 +58,5 @@ if __name__ == "__main__":
     parser.add_argument("--extra-epoch", "-e", type=int, default=0)
     args = parser.parse_args()
 
-    max_steps, params, density_params, _ = auto_hyper_parameter(args.n, args.base, args.extra_epoch)
-    print(" ".join(to_command_args(max_steps, params, density_params)))
+    max_steps, scaled_params, _ = auto_hyper_parameter(args.n, args.base, args.extra_epoch)
+    print(" ".join(to_command_args(max_steps, scaled_params)))
