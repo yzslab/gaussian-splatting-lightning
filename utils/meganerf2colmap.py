dd812c6a1219e0530571d58da1d34871f196d371
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-08-23 18:07:52 +0800

    Update meganerf2colmap.py: add an option to refine the parameters

--- utils/meganerf2colmap.py
+++ utils/meganerf2colmap.py
@@ -18,6 +18,8 @@ def parse_args():
     parser.add_argument("path")
     parser.add_argument("--vocab_tree_path", "-v", type=str,
                         default=os.path.expanduser("~/.cache/colmap/vocab_tree_flickr100K_words256K.bin"))
+    parser.add_argument("--refine", action="store_true",
+                        help="Refine intrinsics and extrinsics")
     args = parser.parse_args()
 
     assert os.path.exists(args.vocab_tree_path), "Vocabulary Tree not found, please download it from 'https://demuc.de/colmap/vocab_tree_flickr100K_words256K.bin', and provide it path via option '-v'"
@@ -27,6 +29,10 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    camera_model = "PINHOLE"
+    if args.refine:
+        camera_model = "OPENCV"
 
     coordinates = torch.load(os.path.join(args.path, "coordinates.pt"), map_location="cpu")
 
@@ -47,7 +53,7 @@ def main():
     os.makedirs(image_dir, exist_ok=True)
     for i, _, image_name in image_metadata_pairs:
         try:
-            os.symlink(i, os.path.join(image_dir, image_name))
+            os.symlink(os.path.realpath(i), os.path.join(image_dir, image_name))
         except FileExistsError:
             pass
 
@@ -57,7 +63,7 @@ def main():
         "feature_extractor",
         "--database_path", colmap_db_path,
         "--image_path", image_dir,
-        "--ImageReader.camera_model", "PINHOLE",
+        "--ImageReader.camera_model", camera_model,
     ]) == 0
 
     colmap_db = sqlite3.connect(colmap_db_path)
@@ -66,6 +72,14 @@ def main():
         cur = colmap_db.cursor()
         try:
             return cur.execute("SELECT image_id, camera_id FROM images WHERE name = ?", [image_name]).fetchone()
+        finally:
+            cur.close()
+
+    def set_image_camera_id(image_id: int, camera_id: int):
+        cur = colmap_db.cursor()
+        try:
+            cur.execute("UPDATE images SET camera_id = ? WHERE image_id = ?", [camera_id, image_id])
+            colmap_db.commit()
         finally:
             cur.close()
 
@@ -79,6 +93,16 @@ def main():
             colmap_db.commit()
         finally:
             cur.close()
+
+    def delete_unused_cameras():
+        cur = colmap_db.cursor()
+        try:
+            cur.execute("DELETE FROM cameras WHERE camera_id NOT IN (SELECT camera_id FROM images)")
+            colmap_db.commit()
+        finally:
+            cur.close()
+
+    camera_intrinsics_to_camera_id = {}
 
     images = {}
     cameras = {}
@@ -99,7 +123,15 @@ def main():
     for _, metadata_path, image_name in image_metadata_pairs:
         metadata = torch.load(metadata_path, map_location="cpu")
         image_id, camera_id = select_image(image_name)
-        update_camera_params(camera_id, metadata["intrinsics"].to(torch.float64).numpy())
+
+        # share intrinsics if possible
+        intrinsics_as_dict_key = metadata["intrinsics"]
+        intrinsics_as_dict_key = torch.concat(
+            [intrinsics_as_dict_key, torch.tensor([metadata["W"], metadata["H"]], dtype=torch.float)],
+            dim=-1,
+        )
+        camera_id = camera_intrinsics_to_camera_id.setdefault(intrinsics_as_dict_key.numpy().tobytes(), camera_id)
+        set_image_camera_id(image_id, camera_id)
 
         c2w = torch.eye(4)
         c2w[:3, :] = metadata["c2w"]
@@ -119,13 +151,22 @@ def main():
             xys=np.array([], dtype=np.float64),
             point3D_ids=np.asarray([], dtype=np.int64),
         )
-        cameras[camera_id] = colmap.Camera(
-            id=camera_id,
-            model="PINHOLE",
-            width=metadata["W"],
-            height=metadata["H"],
-            params=metadata["intrinsics"].to(torch.float64).numpy(),
-        )
+
+        # a new camera
+        if camera_id not in cameras:
+            camera_params = metadata["intrinsics"]
+            if args.refine:
+                camera_params = torch.concat([metadata["intrinsics"], torch.tensor([0., 0., 0., 0.])], dim=-1)
+            update_camera_params(camera_id, camera_params.to(torch.float64).numpy())
+            cameras[camera_id] = colmap.Camera(
+                id=camera_id,
+                model=camera_model,
+                width=metadata["W"],
+                height=metadata["H"],
+                params=camera_params.to(torch.float64).numpy(),
+            )
+
+    delete_unused_cameras()
 
     colmap_db.close()
 
@@ -142,16 +183,44 @@ def main():
         "--VocabTreeMatching.vocab_tree_path", args.vocab_tree_path,
     ]) == 0
 
-    sparse_dir = os.path.join(colmap_dir, "sparse")
-    os.makedirs(sparse_dir, exist_ok=True)
+    sparse_dir_triangulated = os.path.join(colmap_dir, "sparse_triangulated")
+    os.makedirs(sparse_dir_triangulated, exist_ok=True)
     assert subprocess.call([
         "colmap",
         "point_triangulator",
         "--database_path", colmap_db_path,
         "--image_path", image_dir,
         "--input_path", sparse_manually_model_dir,
-        "--output_path", sparse_dir,
+        "--output_path", sparse_dir_triangulated,
     ]) == 0
+
+    if args.refine:
+        # use the intrinsics and extrinsics provided by MegaNeRF will produce a suboptimal result,
+        # so run a bundle adjustment to further refine them
+
+        sparse_dir = os.path.join(colmap_dir, "sparse")
+        os.makedirs(sparse_dir, exist_ok=True)
+        assert subprocess.call([
+            "colmap",
+            "bundle_adjuster",
+            "--input_path", sparse_dir_triangulated,
+            "--output_path", sparse_dir,
+        ]) == 0
+
+        dense_dir = os.path.join(colmap_dir, "dense")
+        os.makedirs(dense_dir, exist_ok=True)
+        assert subprocess.call([
+            "colmap",
+            "image_undistorter",
+            "--image_path", image_dir,
+            "--input_path", sparse_dir,
+            "--output_path", dense_dir,
+            "--max_image_size", "1600",
+        ])
+        print("Saved to '{}', use this as your dataset path".format(dense_dir))
+    else:
+        os.rename(sparse_dir_triangulated, os.path.join(colmap_dir, "sparse"))
+        print("Saved to '{}', use this as your dataset path".format(colmap_dir))
 
 
 def test_main():
