96773d0edd511eb6297edfac859956a7626d2e9f
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-10-24 10:56:49 +0800

    Add scale regularization metrics

---
+++ internal/metrics/scale_regularization_metrics.py
@@ -0,0 +1,108 @@
+from dataclasses import dataclass
+from typing import Tuple, Dict, Any
+import torch
+from .vanilla_metrics import VanillaMetrics, VanillaMetricsImpl
+
+
+@dataclass
+class ScaleRegularizationMetricsMixin:
+    """Avoid large and highly anisotropic Gaussians"""
+
+    scale_reg_from: int = -1
+    """Should be at least after the first opacity reset + a densify interval, i.e. 3100 by default"""
+
+    max_scale: float = -1
+
+    max_scale_ratio: float = -1
+
+    scale_reg_lambda: float = 0.05
+
+    scale_ratio_reg_lambda: float = 0.05
+
+    def __post_init__(self):
+        if hasattr(super(), "__post_init__"):
+            super().__post_init__()
+
+        assert self.scale_reg_from > 0
+        assert self.max_scale > 0
+        assert self.max_scale_ratio > 0
+
+    def instantiate(self, *args, **kwargs) -> "ScaleRegularizationMetricsModuleMixin":
+        raise NotImplementedError()
+
+
+class ScaleRegularizationMetricsModuleMixin:
+    def get_scale_regularization_metrics(self, gaussian_model, metrics, pbar):
+        scales = gaussian_model.get_scales()
+        sorted_scales = torch.sort(scales, dim=-1).values
+
+        max_scales = sorted_scales[:, -1]
+        mid_scales = sorted_scales[:, -2]
+
+        over_scales = max_scales - self.config.max_scale
+        is_over_scales = over_scales.detach() > 0.
+        n_over_scales = is_over_scales.sum()
+
+        scale_ratios = max_scales / (mid_scales + 1e-8)
+        over_ratios = scale_ratios - self.config.max_scale_ratio
+        is_over_ratios = over_ratios.detach() > 0.
+        n_over_ratios = is_over_ratios.sum()
+
+        over_scale_loss = (over_scales * is_over_scales).sum() / (n_over_scales + 1) * self.config.scale_reg_lambda
+        over_ratio_loss = (over_ratios * is_over_ratios).sum() / (n_over_ratios + 1) * self.config.scale_ratio_reg_lambda
+
+        metrics["loss"] = metrics["loss"] + over_scale_loss + over_ratio_loss
+        metrics["scale_reg"] = over_scale_loss
+        metrics["scale_ratio_reg"] = over_ratio_loss
+        metrics["n_over_scales"] = n_over_scales
+        metrics["n_over_ratios"] = n_over_ratios
+        metrics["max_scale"] = max_scales.max()
+        metrics["max_ratio"] = scale_ratios.max()
+        metrics["mean_ratio"] = scale_ratios.mean()
+
+        pbar["scale_reg"] = False
+        pbar["scale_ratio_reg"] = False
+        pbar["n_over_scales"] = False
+        pbar["n_over_ratios"] = False
+        pbar["max_scale"] = False
+        pbar["max_ratio"] = False
+        pbar["mean_ratio"] = False
+
+        return sorted_scales, over_scales, is_over_scales, over_ratios, is_over_ratios
+
+    def get_train_metrics(
+            self,
+            pl_module,
+            gaussian_model,
+            step: int,
+            batch,
+            outputs,
+    ) -> Tuple[Dict[str, Any], Dict[str, bool]]:
+        metrics, pbar = super().get_train_metrics(
+            pl_module,
+            gaussian_model,
+            step,
+            batch,
+            outputs,
+        )
+
+        if step >= self.config.scale_reg_from:
+            self.get_scale_regularization_metrics(gaussian_model, metrics, pbar)
+
+        return metrics, pbar
+
+    def get_validate_metrics(self, pl_module, gaussian_model, batch, outputs) -> Tuple[Dict[str, Any], Dict[str, bool]]:
+        metrics, pbar = super().get_validate_metrics(pl_module, gaussian_model, batch, outputs)
+        self.get_scale_regularization_metrics(gaussian_model, metrics, pbar)
+
+        return metrics, pbar
+
+
+@dataclass
+class ScaleRegularizationMetrics(ScaleRegularizationMetricsMixin, VanillaMetrics):
+    def instantiate(self, *args, **kwargs) -> "ScaleRegularizationMetricsModuleMixin":
+        return ScaleRegularizationMetricsModule(self)
+
+
+class ScaleRegularizationMetricsModule(ScaleRegularizationMetricsModuleMixin, VanillaMetricsImpl):
+    pass
