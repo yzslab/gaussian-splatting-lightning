2f5fdad7a8ba08382394bf0b1ad2c22ce1fe2c46
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2024-10-24 16:56:38 +0800

    Update scale regularization

--- internal/metrics/scale_regularization_metrics.py
+++ internal/metrics/scale_regularization_metrics.py
@@ -12,6 +12,7 @@ class ScaleRegularizationMetricsMixin:
     """Should be at least after the first opacity reset + a densify interval, i.e. 3100 by default"""
 
     max_scale: float = -1
+    """Should be greater than `percent_dense * camera_extent`, or splitting of densification will not work"""
 
     max_scale_ratio: float = -1
 
@@ -39,26 +40,25 @@ class ScaleRegularizationMetricsModuleMixin:
         max_scales = sorted_scales[:, -1]
         mid_scales = sorted_scales[:, -2]
 
-        over_scales = max_scales - self.config.max_scale
-        is_over_scales = over_scales.detach() > 0.
+        is_over_scales = scales.detach() > self.config.max_scale
         n_over_scales = is_over_scales.sum()
 
         scale_ratios = max_scales / (mid_scales + 1e-8)
-        over_ratios = scale_ratios - self.config.max_scale_ratio
-        is_over_ratios = over_ratios.detach() > 0.
+        is_over_ratios = scale_ratios.detach() > self.config.max_scale_ratio
         n_over_ratios = is_over_ratios.sum()
 
-        over_scale_loss = (over_scales * is_over_scales).sum() / (n_over_scales + 1) * self.config.scale_reg_lambda
-        over_ratio_loss = (over_ratios * is_over_ratios).sum() / (n_over_ratios + 1) * self.config.scale_ratio_reg_lambda
+        over_scale_loss = (scales * is_over_scales).sum() / (n_over_scales + 1) * self.config.scale_reg_lambda
+        over_ratio_loss = (scale_ratios * is_over_ratios).sum() / (n_over_ratios + 1) * self.config.scale_ratio_reg_lambda
 
         metrics["loss"] = metrics["loss"] + over_scale_loss + over_ratio_loss
         metrics["scale_reg"] = over_scale_loss
         metrics["scale_ratio_reg"] = over_ratio_loss
         metrics["n_over_scales"] = n_over_scales
         metrics["n_over_ratios"] = n_over_ratios
-        metrics["max_scale"] = max_scales.max()
-        metrics["max_ratio"] = scale_ratios.max()
-        metrics["mean_ratio"] = scale_ratios.mean()
+        with torch.no_grad():
+            metrics["max_scale"] = max_scales.max()
+            metrics["max_ratio"] = scale_ratios.max()
+            metrics["mean_ratio"] = scale_ratios.mean()
 
         pbar["scale_reg"] = False
         pbar["scale_ratio_reg"] = False
@@ -68,7 +68,7 @@ class ScaleRegularizationMetricsModuleMixin:
         pbar["max_ratio"] = False
         pbar["mean_ratio"] = False
 
-        return sorted_scales, over_scales, is_over_scales, over_ratios, is_over_ratios
+        return sorted_scales, is_over_scales, is_over_ratios
 
     def get_train_metrics(
             self,
