de622ccc11caeb03db2f0c4441826dad2eeea721
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2025-08-05 19:22:47 +0800

    Fix initialization

--- internal/output_processors/exposure.py
+++ internal/output_processors/exposure.py
@@ -12,7 +12,13 @@ class ExposureProcessor(OutputProcessor):
 
     max_steps: int = 30_000
 
+    max_gray_scale: float = 5.
+
+    max_gamma: float = 5.
+
     def instantiate(self, *args, **kwargs):
+        assert self.max_gray_scale > 1.
+        assert self.max_gamma > 1.
         return ExposureProcessorModule(self)
 
 
@@ -33,13 +39,13 @@ class ExposureProcessorModule(torch.nn.Module):
 
         exposures = torch.full(
             (n_appearances, 4),
-            inverse_sigmoid(torch.tensor(1., dtype=torch.float32)),
+            inverse_sigmoid(torch.tensor(1. / self.config.max_gray_scale, dtype=torch.float32)),
             dtype=torch.float32,
             device=pl_module.device,
         )
-        exposures[:, -1] = inverse_sigmoid(torch.tensor(0.1, dtype=torch.float32, device=pl_module.device))
-        assert torch.all(torch.sigmoid(exposures[:, :3]) == 1.)
-        assert torch.all(torch.sigmoid(exposures[:, -1]) == 0.1)
+        exposures[:, -1] = inverse_sigmoid(torch.tensor(1. / self.config.max_gamma, dtype=torch.float32, device=pl_module.device))
+        assert torch.allclose(torch.sigmoid(exposures[:, :3]) * self.config.max_gray_scale, torch.tensor(1., device=pl_module.device))
+        assert torch.allclose(torch.sigmoid(exposures[:, -1]) * self.config.max_gamma, torch.tensor(1., device=pl_module.device))
         self.exposure_parameters = torch.nn.Parameter(exposures, requires_grad=True)
 
 
@@ -65,6 +71,7 @@ class ExposureProcessorModule(torch.nn.Module):
         adjustment = torch.sigmoid(self.exposure_parameters[camera.appearance_id])
 
         rendered_image = outputs["render"]  # [C, H, W]
-        rendered_image = adjustment[:3, None, None] * rendered_image
-        rendered_image = torch.pow(rendered_image + 1e-5, adjustment[-1] * 10)
+        rendered_image = adjustment[:3, None, None] * self.config.max_gray_scale * rendered_image
+        rendered_image = torch.pow(rendered_image + 1e-5, adjustment[-1] * self.config.max_gamma)
+        rendered_image = torch.clamp_max(rendered_image, max=1.)
         outputs["render"] = rendered_image
