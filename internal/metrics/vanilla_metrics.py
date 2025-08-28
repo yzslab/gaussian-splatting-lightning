2d57062f52d4203855543f7b804ad0cb8eb0eb85
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2025-08-28 23:00:27 +0800

    Fix PSNR data range

--- internal/metrics/vanilla_metrics.py
+++ internal/metrics/vanilla_metrics.py
@@ -40,7 +40,7 @@ class VanillaMetricsImpl(MetricImpl):
 
 
     def setup(self, stage: str, pl_module):
-        self.psnr = PeakSignalNoiseRatio()
+        self.psnr = PeakSignalNoiseRatio(data_range=1.0)
         self.no_state_dict_models["lpips"] = LearnedPerceptualImagePatchSimilarity(normalize=True, net_type=self.config.lpips_net_type)
 
         self.lambda_dssim = self.config.lambda_dssim
